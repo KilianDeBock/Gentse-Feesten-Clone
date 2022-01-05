221d3b3610eb586510297393d4244dd2cf3e238d
Kilian De Bock <Stualyttle@users.noreply.github.com>
2022-01-05 20:55:23 +0100

    22.1.3.3: Started on hamburger and added program and its styles. Updated arrow svg. Added hover effect a elements.

--- static/js/main.js
+++ static/js/main.js
@@ -9,11 +9,15 @@
       this.$elements = document.querySelectorAll('selector');
       this.$hamburger = document.querySelector('.hamburger');
       this.$hamburgerContent = document.querySelector('.header__navigation__mobile_content');
+      this.$hamburgerProgram = document.querySelector('#hamburger_program');
     },
     addEventListeners() {
       this.$hamburger.addEventListener('click', () => {
         this.$hamburger.classList.toggle('close');
         this.$hamburgerContent.classList.toggle('hidden');
+      });
+      this.$hamburgerProgram.addEventListener('click', (ev) => {
+        this.$hamburgerProgram.classList.toggle('close');
       });
     },
     generateUI() {
