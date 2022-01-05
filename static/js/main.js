b62ea16003480f89e933344a29179819b0e192fc
Kilian De Bock <Stualyttle@users.noreply.github.com>
2022-01-05 16:31:37 +0100

    22.1.3.2: Updated svg colors, started working on mobile version and added hamburger.

--- static/js/main.js
+++ static/js/main.js
@@ -2,11 +2,19 @@
   const app = {
     initialize() {
       this.cacheElements();
+      this.addEventListeners();
       this.generateUI();
     },
     cacheElements() {
       this.$elements = document.querySelectorAll('selector');
-      this.$element = document.querySelector('selector');
+      this.$hamburger = document.querySelector('.hamburger');
+      this.$hamburgerContent = document.querySelector('.header__navigation__mobile_content');
+    },
+    addEventListeners() {
+      this.$hamburger.addEventListener('click', () => {
+        this.$hamburger.classList.toggle('close');
+        this.$hamburgerContent.classList.toggle('hidden');
+      });
     },
     generateUI() {
     }
