9241a8f3a8f5a2522fedf0751fd44d491644f9ed
Kilian De Bock <Stualyttle@users.noreply.github.com>
2022-01-12 13:04:11 +0100

    22.2.3.1: Added days active script and styling, fixed footer image.

--- static/js/pages/day.js
+++ static/js/pages/day.js
@@ -1,14 +1,25 @@
 (() => {
   const day = {
     initialize() {
+      // Create new url object
+      this.url = new URL(document.URL);
+      // Get the day, or set default value.
+      this.setDay = this.url.searchParams.get('day') ?? '19';
+
       this.cacheElements();
-      this.generateUI();
+      this.setActiveDay();
     },
     cacheElements() {
-      this.$elements = document.querySelectorAll('selector');
-      this.$element = document.querySelector('selector');
+      this.$days = document.querySelectorAll('header .days li');
+      // this.$element = document.querySelector('selector');
     },
-    generateUI() {
+    setActiveDay() {
+      this.$days.forEach(day => {
+        day.classList.remove('active');
+        if (day.id === `day_${this.setDay}`) {
+          day.classList.add('active');
+        }
+      });
     }
   };
   // Start initialization.
